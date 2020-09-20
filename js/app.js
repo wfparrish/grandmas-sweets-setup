00e1c5532cc08d29a66564c2c2f6d77279455eee
wfparrish <williparrish@gmail.com>
2020-09-20 08:13:51 -0600

    made changes to About Us section

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-document.getElementById("cart-info").addEventListener("click", function() {
-  const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
-});
+// document.getElementById("cart-info").addEventListener("click", function () {
+//   const cart = document.getElementById("cart");
+//   cart.classList.toggle("show-cart");
+//   console.log(cart);
+// });
